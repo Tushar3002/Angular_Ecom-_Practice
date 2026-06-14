2f08e6bdc5144ad0e2364b5f5e3a344c744b21a9
Tushar <tusharjana.aj3@gmail.com>
2026-06-15 00:33:35 +0530

    made a  basic

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,32 @@
+import { Component, signal } from '@angular/core';
+import { Product } from '../../core/models/product';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  products= signal<Product[]>([]);
+  constructor(
+    
+    private router: Router,
+    private http: HttpClient,
+  ) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  url = 'https://fakestoreapi.com/products';
+  getProducts() {
+    this.http.get<Product[]>(this.url).subscribe((data) => {
+      this.products.set(data);
+      console.log(data);
+      
+    });
+  }
+}
